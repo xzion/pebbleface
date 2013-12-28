2244edd6c1e6ed9f0dc389c22abc56311ff48050
xzion <coen.mcclelland@gmail.com>
2013-12-28 05:55:34 -0800

    First Commit

---
+++ src/xzion1.c
@@ -0,0 +1,47 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  static char boundtext[20];
+
+  snprintf(boundtext, 20, "w=%d, h=%d", bounds.size.w, bounds.size.h);
+
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, boundtext);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
